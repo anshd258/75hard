f325e8881930b1604cb636d4472c1d6dccfdf7bd
anshd258 <anshd258@gmail.com>
2024-03-12 23:50:50 +0530

    Max Consecutive Ones III in cpp

---
+++ MaxConsecutiveOnesIII.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+     int i=0,j=0;
+        while(j<nums.size()){
+            if(nums[j]==0){
+                k--;
+            }
+            if(k<0){
+                if(nums[i]==0){
+                    k++;
+                }
+                i++;
+            }
+            j++;
+        }
+        return j-i;
+    }
+};
+
+int main(){
+       int x[] = {0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1};
+    vector<int> sample(x, x + sizeof x / sizeof x[0]) ;
+     auto result = Solution().increasingTriplet(sample);
+      
+        cout << result;
+   
+}
