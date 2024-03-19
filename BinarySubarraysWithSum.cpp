25bc01496483995b71d6549f77c409b6353c8490
anshd258 <anshd258@gmail.com>
2024-03-19 22:08:12 +0530

    added Climbing Stairs in cpp

--- BinarySubarraysWithSum.cpp
+++ BinarySubarraysWithSum.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <unordered_map>
 #include <vector>
-
+using namespace std;
 class Solution {
 public:
-    int numSubarraysWithSum(std::vector<int>& nums, int goal) {
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
         int result = 0;
         int sum = 0;
-        std::unordered_map<int, int> order;
+        unordered_map<int, int> order;
         order[0] = 1;
 
         for (int i = 0; i < nums.size(); ++i) {
@@ -22,8 +22,8 @@ public:
 
 int main() {
     Solution solution;
-    std::vector<int> nums = {1, 0, 1, 0, 1}; // Example input
+ vector<int> nums = {1, 0, 1, 0, 1}; // Example input
     int goal = 2; // Example goal
-    std::cout << "Number of subarrays: " << solution.numSubarraysWithSum(nums, goal) << std::endl;
+    cout << "Number of subarrays: " << solution.numSubarraysWithSum(nums, goal) << endl;
     return 0;
 }
